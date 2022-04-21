54bc058478e35d70ada1d65e2eb5f82a96eeef67
Artur Baryspiev <oxygen12ium3@gmail.com>
2022-04-21 21:44:08 +0300

    add 02_task

---
+++ module4/02_task/cmd/myapp.go
@@ -0,0 +1,13 @@
+package cmd
+
+//package main
+
+import (
+	"fmt"
+	"module04/02_task/internal"
+)
+
+func main() {
+	customer := internal.NewCustomer("Dmitry", "23", 10_000, 0, true)
+	fmt.Println(internal.CalcPrice(*customer, 800))
+}
---
+++ module4/02_task/internal/Customer.go
@@ -0,0 +1,42 @@
+package internal
+
+import "errors"
+
+const DEFAULT_DISCOUNT = 500
+
+type Customer struct {
+	Name     string
+	Age      string
+	Balance  int
+	Debt     int
+	Discount bool
+}
+
+func (customer *Customer) CalcDiscount() (int, error) {
+	if !customer.Discount {
+		return 0, errors.New("discount not available")
+	}
+	result := DEFAULT_DISCOUNT - customer.Debt
+	if result < 0 {
+		return 0, nil
+	}
+	return result, nil
+}
+
+func NewCustomer(Name, Age string, Balance, Debt int, Discount bool) *Customer {
+	return &Customer{
+		Name:     Name,
+		Age:      Age,
+		Balance:  Balance,
+		Debt:     Debt,
+		Discount: Discount,
+	}
+}
+
+func CalcPrice(customer Customer, price int) (int, error) {
+	discount, err := customer.CalcDiscount()
+	if err != nil {
+		return 0, err
+	}
+	return price - discount, nil
+}
